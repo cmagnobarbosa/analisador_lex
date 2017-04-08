930d4d9916c91d48dbc797aa13c79cfe14ded634
Exterminus <cmagnobarbosa@gmail.com>
2017-04-08 17:25:01 -0300

    funcionamento basico

--- anlex.py
+++ anlex.py
@@ -2,73 +2,90 @@
 import sys
 import re
 
+
+def converte_simbolos():
+    """Substitui simbolos por números"""
+    pass
+
+
 def gera_marcador(elemento):
     if elemento is " ":
         return "|Sep, Space"
     else:
-        return "|Sep, "+str(elemento)
+        print "Tipo ", type(elemento)
+        return "|Sep, " + elemento + "|"
+
 
 def formata_tokens(tokens):
-    novos=tokens.split("|")
+    print tokens
+    novos = tokens.split("|")
     for i in novos:
-        print "<",i,">"
+        print "<", i, ">"
 arquivo = open("teste.c", "r")
 
 token = ""
 numerico = ""
 estado = 0
-separadores = [';', '\\', '\n','[', ']', ')', '(', ')', '{', '}', ',', '=']
+separadores = [';', '[', ']', ')', '(', ')', '{', '}', ',', '=', '.']
+sep_num = [';',',', '=']
 operadores = ['-', '+', '/', '*']
 op_log = ['&&', '||', '>', '<', '>=', '<=', '==', '!=']
 lista_erros = []
-Identificador=[]
+Identificador = []
 linha = 0
-final =""
+final = ""
 for i in arquivo:
     linha = linha + 1
     final = ""
     for k in i:
-        # print "Letra ",k
+
         if estado is 0:
             """Define o estado inicial"""
             if re.match(r"([A-Za-z_])", k):
-                #token = token + k
                 estado = 1  # Identificador
-
             if re.match(r"[0-9]", k):
-                    estado = 2  # Constante Numerica
-            if k in operadores:
-                print "entrou"
-                print k
-        if estado is 1:
 
+                estado = 2  # Constante Numerica
+            if k in operadores:
+                print "entrouu"
+                token = token + k + "|"
+                estado = 0
+
+        if estado is 1:
             """Valida Identificador"""
-            if re.match(r"([\w])", k):
+            if re.match(r"([A-za-z0-9])", k):
                 token = token + k
-            if k in separadores or re.match(r"(\s)",k):
+            if k in separadores or re.match(r"(\s)", k):
                 """Lista com separadores"""
                 estado = 0
-                token = "|Iden, "+token + ", L:"+str(linha) + gera_marcador(k)+ ", L:" + str(linha)+"|"
+                print "Valor presente em k:" , k
+                token = "|Iden, " + token + ", L:" + \
+                    str(linha) + gera_marcador(k) + ", L:" + str(linha) + "|"
 
         if estado is 2:
             """Estado de indentificacao de constante numerica"""
             if re.match(r"[\w.]", k):
-                numerico = numerico + k
-            else:
-                final = final + str(k)+", L:" + str(linha)+"|"
-            if k in separadores or operadores:
 
-                if(re.match(r"(^[0-9]*$|[0-9].[0-9]+)",numerico)):
-                    valor = re.match(r"(^[0-9]*$|[0-9].[0-9]+)",numerico)
+                numerico = numerico + k
+            if k in sep_num or k in operadores:
+
+                if(re.match(r"(^[0-9]*$|[0-9]+.[0-9]+)", numerico)):
+                    valor = re.match(r"(^[0-9]*$|[0-9]+.[0-9]+)", numerico)
                     if valor is not None:
-                        token = token + "|Num, "+valor.group()+",L:"+str(linha)+"|"
+                        token = token + "Num, " + valor.group() + ",L:" + str(linha) + gera_marcador(k)
                         estado = 0
                         numerico = ""
                 else:
-                    lista_erros.append([numerico,linha])
+                    print "erro"
+                    lista_erros.append([numerico, linha])
                     numerico = ""
-    token = token + final
+            else:
+                if k in sep_num:
+                    print "valor"
+                    "Armazena token de separadores"
+                    token = token + "Sep," + k + ",L:" + str(linha) + "|"
+
 
 formata_tokens(token)
 print lista_erros
-#print "Erro", lista_erros
+# print "Erro", lista_erros
---
+++ teste2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int c=0;
+  int.b;
+  printf("%d\n",c);
+  return 0;
+}
