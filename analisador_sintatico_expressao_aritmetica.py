caf8cf727d66a363415c52b82371f06d54d4224a
Exterminus <cmagnobarbosa@gmail.com>
2017-06-04 20:16:27 -0300

    Versao 1 do analisador arimetico funcionando

--- analisador_sintatico_expressao_aritmetica.py
+++ analisador_sintatico_expressao_aritmetica.py
@@ -1,59 +1,79 @@
+# coding: utf-8
 """
 Analisador Sintático
 Carlos Magno
 
 """
-tokens = ['$',"(","b","+", "a","+","b",")"]
+tokens = "(a+a)$"
 
 
-def E(simb,lista):
-    print simb
+def E(simb, lista, pos):
+    # print simb
     if(simb == 'a' or simb == "("):
-
-        T(simb,lista)
-
-        Elinha(simb,lista)
+        T(simb, lista, pos)
+        Elinha(simb, lista, pos)
     else:
+        print "Erro Posicão",pos,"Caracter ",lista[pos]
+        exit()
 
-        print "Erro E"
 
-
-def T(simb, lista):
+def T(simb, lista, pos):
 
     if(simb == "a" or simb == "("):
 
-        F(simb, lista)
-        Elinha(simb, lista)
+        F(simb, lista, pos)
+        Elinha(simb, lista, pos)
     else:
+        #Elinha(simb, lista, pos)
+        # print "simboloT ", simb, pos
+        print "Erro Posicão",pos,"Caracter ",lista[pos]
+        exit()
 
-        print "Erro T"
 
+def F(simb, lista, pos):
 
-def F(simb,lista):
     if(simb == "("):
-        E(simb,lista)
-        simb = get_next_token(lista)
+
+        retorno = get_next_token(lista, pos)
+        simb = retorno[0]
+        pos = retorno[1]
+        E(simb, lista, pos)
         if(simb != ")"):
-            print "Erro F"
+            print "Erro Posicão",pos,"Caracter ",lista[pos]
+            exit()
     elif(simb == "a"):
-        simb = get_next_token(lista)
+        retorno = get_next_token(lista, pos)
+        simb = retorno[0]
+        pos = retorno[1]
+        Elinha(simb, lista, pos)
 
     else:
-        print "Erro F"
-def get_next_token(lista):
-    return lista.pop()
+        print "Erro Posicão",pos,"Caracter ",lista[pos]
+        exit()
 
-def Elinha(simb,lista):
+
+def get_next_token(lista, pos):
+
+    pos += 1
+    # print "Retorno ", lista[pos], pos
+    return lista[pos], pos
+
+
+def Elinha(simb, lista, pos):
+    # print"Elinha ", simb, pos
     if(simb == "+"):
-        simb = get_next_token(lista)
-        print "simbolo ",simb
-        T(sim,lista)
-        Elinha(simb,lista)
+        retorno = get_next_token(lista, pos)
+        simb = retorno[0]
+        pos = retorno[1]
+        # print "simbolo2 ", simb, pos
+        T(simb, lista, pos)
+        #Elinha(simb, lista, pos)
     elif (simb == ")" or simb == "$"):
         print "valido"
     else:
-        print "Simbolo ",simb, "Lista ",lista
-        print "erro Elinha"
+        # print simb
+        #print "Erro Posicão",pos,"Caracter ",lista[pos]
+        exit()
 
-tokens.reverse()
-Elinha(tokens.pop(),tokens)
+
+E(tokens[0], tokens, 0)
