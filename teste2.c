42654d759591d49a9d8c529463dbed989491e5dc
Exterminus <cmagnobarbosa@gmail.com>
2017-06-08 21:11:40 -0300

    atualização do teste

--- teste2.c
+++ teste2.c
@@ -5,12 +5,7 @@ Arquivo de teste
 */
 
 int main() {
-  8int d=0;
-  int c=0;
-  float a=7.77;
-  for(d=0;d<10;d++){
-    printf("%d\n",d);
-      $
-  }
+  int a =8;
+  printf("%d",a/2);
   return 0;
 }
