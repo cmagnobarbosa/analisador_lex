d3d6128205ed8c3fd1446481ca61b27f4aeeb157
Exterminus <cmagnobarbosa@gmail.com>
2017-06-08 21:12:18 -0300

    Melhoria da função de análise aritmética

--- analisador_sintatico_expressao_aritmetica.py
+++ analisador_sintatico_expressao_aritmetica.py
@@ -4,7 +4,7 @@ Analisador Sintático
 Carlos Magno
 
 """
-tokens = "(a+a)$"
+tokens = "(a-a+a//a)"
 
 
 def E(simb, lista, pos):
@@ -13,7 +13,7 @@ def E(simb, lista, pos):
         T(simb, lista, pos)
         Elinha(simb, lista, pos)
     else:
-        print "Erro Posicão",pos,"Caracter ",lista[pos]
+        print "Erro Caracter Posicao ", pos, simb
         exit()
 
 
@@ -22,12 +22,12 @@ def T(simb, lista, pos):
     if(simb == "a" or simb == "("):
 
         F(simb, lista, pos)
-        Elinha(simb, lista, pos)
+        Tlinha(simb, lista, pos)
     else:
         #Elinha(simb, lista, pos)
         # print "simboloT ", simb, pos
-        print "Erro Posicão",pos,"Caracter ",lista[pos]
-        exit()
+        print "Erro Caracter Posicao ", pos, simb
+        return 0
 
 
 def F(simb, lista, pos):
@@ -39,7 +39,7 @@ def F(simb, lista, pos):
         pos = retorno[1]
         E(simb, lista, pos)
         if(simb != ")"):
-            print "Erro Posicão",pos,"Caracter ",lista[pos]
+            print "Erro Caracter Posicao ", pos
             exit()
     elif(simb == "a"):
         retorno = get_next_token(lista, pos)
@@ -48,19 +48,18 @@ def F(simb, lista, pos):
         Elinha(simb, lista, pos)
 
     else:
-        print "Erro Posicão",pos,"Caracter ",lista[pos]
+        print "Erro Caracter Posicao ", pos, simb
         exit()
 
 
 def get_next_token(lista, pos):
-
+    """Puxa o proximo token"""
     pos += 1
-    # print "Retorno ", lista[pos], pos
     return lista[pos], pos
 
 
 def Elinha(simb, lista, pos):
-    # print"Elinha ", simb, pos
+    """Válida adição e subtração."""
     if(simb == "+"):
         retorno = get_next_token(lista, pos)
         simb = retorno[0]
@@ -69,11 +68,39 @@ def Elinha(simb, lista, pos):
         T(simb, lista, pos)
         #Elinha(simb, lista, pos)
     elif (simb == ")" or simb == "$"):
-        print "valido"
+        print "Válido"
+    elif (simb == "-"):
+        retorno = get_next_token(lista, pos)
+        simb = retorno[0]
+        pos = retorno[1]
+        # print "simbolo2 ", simb, pos
+        T(simb, lista, pos)
     else:
-        # print simb
-        #print "Erro Posicão",pos,"Caracter ",lista[pos]
+        Tlinha(simb, lista, pos)
+        # return 0
+        # print "Invalido"
         exit()
 
+
+def Tlinha(simb, lista, pos):
+    """Válida multiplicação"""
+    # print"Elinha ", simb, pos
+    if(simb == "*"):
+        retorno = get_next_token(lista, pos)
+        simb = retorno[0]
+        pos = retorno[1]
+        # print "simbolo2 ", simb, pos
+        F(simb, lista, pos)
+        Tlinha(simb, lista, pos)
+    elif(simb == "/"):
+        retorno = get_next_token(lista, pos)
+        simb = retorno[0]
+        pos = retorno[1]
+        F(simb, lista, pos)
+    elif (simb == ")" or simb == "$"):
+        print "Válido"
+    else:
+        return 0
+        exit()
 
 E(tokens[0], tokens, 0)
